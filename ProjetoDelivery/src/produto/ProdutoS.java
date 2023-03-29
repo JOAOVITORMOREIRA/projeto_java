1c2fffe601f660b1180abceef0237d0fd2a24485
João Vitor Moreira <joaoitagv@gmail.com>
2023-03-29 15:52:03 -0300

    Tentativa de commitar

---
+++ ProjetoDelivery/src/produto/ProdutoS.java
@@ -0,0 +1,32 @@
+package produto;
+
+public class ProdutoS {
+	
+	private String nome;
+    private double preco;
+
+    public void Produto (String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void visualizar() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Preço: " + preco);
+    }
+
+	
+
+}
